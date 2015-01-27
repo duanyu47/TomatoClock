46a2ab3b298f528f1892f586cb2a93c85c1b8854
duanyu47 <duanyu47@gmail.com>
2015-01-27 10:39:27 +0800

    init
    init

---
+++ app/src/main/java/me/duanyu/tomatoclock/MyApplication.java
@@ -0,0 +1,13 @@
+package me.duanyu.tomatoclock;
+
+import android.app.Application;
+
+/**
+ * Created by Steve on 2015/1/27.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
